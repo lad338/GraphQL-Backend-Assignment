aa824831957a9fc1f42ce5c16565f3d7a7b1598f
Dicky Li <dickyli338@gmail.com>
2020-06-01 12:01:40 +0800

    initialize

---
+++ src/main/kotlin/com/gtomato/backend/assignment/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.gtomato.backend.assignment
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(GraphQlBackendAssignmentApplication::class.java)
+	}
+
+}
